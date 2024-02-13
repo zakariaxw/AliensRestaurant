e101995a8d69fae7366a7c7e0eede5724ae51c91
zakariaxw <zakariajr963@gmail.com>
2024-02-13 08:51:18 +0100

    zack edition

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 const addUser = require("./addUser");
 const showusers = require("./showusers");
+const edituser = require("./edituser");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -10,11 +11,9 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/home.html");
 });
 
-
 app.post("/adduser", addUser);
-
-
 app.get("/showusers", showusers);
+app.put("/edituser", edituser);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ edituser.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+
+function edituser(req, res) {}
+
+module.exports = edituser;
